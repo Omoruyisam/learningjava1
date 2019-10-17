2e87f2cc53fffe1b9d6a13eb9ea5093ab73fcbac
omoru <omoru@LAPTOP-O7QNCOVM>
2019-10-16 23:54:55 -0400

    new commit

---
+++ src/Learningjava1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Learningjava1 {
+    public static void main(String[] args) {
+ int a = (int) Math.abs(Math.min(-6, 3));
+ 
+ System.out.println(a);
+    }
+}
+
+        
+
+    
+    
+
